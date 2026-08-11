2f504dcfbbbabb01675255ff306fd58b55e9fcc6
anujcreater-84 <anuj.creater84@gmail.com>
2026-08-11 20:15:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Supabase public browser configuration. NEVER put the service_role/secret key here.
 const SUPABASE_URL = "https://xnhxsiipghssmurbixpf.supabase.co";
 
-const SUPABASE_PUBLISHABLE_KEY = "YOUR_PUBLISHABLE_KEY";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_Gv5U9xY_L1F6Y1FLqPa8Wg_XLDrZm4b";
 
 const WHATSAPP_NUMBER = "919999999999";
